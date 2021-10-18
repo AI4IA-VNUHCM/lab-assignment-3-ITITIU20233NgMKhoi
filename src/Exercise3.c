09f788b911da32c784bc7e5b9bc59615dc946004
ITITIU20233NgMKhoi <ititiu20233@student.hcmiu.edu.vn>
2021-10-18 16:37:14 +0700

    uppppp

--- src/Exercise3.c
+++ src/Exercise3.c
@@ -12,18 +12,52 @@ Ex:
 #include <stdlib.h>
 #include <math.h>
 
-void Ex3(int arr[], int n){
+void Ex3(int in_arr[], int n){
 	//Your codes here
-	int max = 1, len = 1, maxIndex = 0; 
-	for (int i=1;i<n; i++) {
-if (arr[i] > arr[i-1]) len++; 
-else { if (max < len) {max = len; maxIndex =i-max; } 
-len = 1; } } 
-if (max < len){ max = len; maxIndex = n - max; }
-for (int i=maxIndex; i<max+maxIndex; i++)
-printf("%d",arr[i]); int main();
-int arr[n]; for(int i=0;i<n;i++) 
-Ex3(arr, n); return 0; }
+	int max_increase = 1, count_increase = 1, increase_index = 0;
+	int max_decrease = 1, count_decrease = 1, decrease_index = 0;
+	for(int i = 1; i < n; i++) {
+		if(in_arr[i] > in_arr[i-1]) {
+			count_increase++;
+		} 
+		else {
+			if(max_increase < count_increase) {
+				max_increase = count_increase;
+				increase_index = i - max_increase;
+			}
+			count_increase = 1;
+		}
+	}
+	if(max_increase < count_increase) {
+		max_increase = count_increase;
+		increase_index = n - max_increase;
+	}
+	for(int j = 1; j < n; j++) {
+		if(in_arr[j] < in_arr[j-1]) {
+			count_decrease++;
+		}
+		else {
+			if(max_decrease < count_decrease) {
+				max_decrease = count_decrease;
+				decrease_index = j - max_decrease;
+			}
+			count_decrease = 1;
+		}
+	}
+	if(max_decrease < count_decrease) {
+		max_decrease = count_decrease;
+		decrease_index = n - max_decrease;
+	}
+	printf("Increasing ");
+	for(int i = increase_index; i < max_increase + increase_index; i++) {
+		printf("%d ", in_arr[i]);
+	}
+	printf("Decreasing ");
+	for(int j = decrease_index; j < max_decrease + decrease_index; j++) {
+		printf("%d ", in_arr[j]);
+	}
+}
+
 int main(int argc, char *argv[]) {
 	//testing variable, applying it to your algorithm for auto-evaluating
 	argc--;
