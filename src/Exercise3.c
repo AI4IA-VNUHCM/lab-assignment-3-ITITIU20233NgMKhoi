7008e48dac72143dd8106b222b64114719cd0565
ITITIU20233NgMKhoi <ititiu20233@student.hcmiu.edu.vn>
2021-10-18 16:17:26 +0700

    Update Exercise3.c

--- src/Exercise3.c
+++ src/Exercise3.c
@@ -12,11 +12,18 @@ Ex:
 #include <stdlib.h>
 #include <math.h>
 
-void Ex3(int in_arr[], int n){
+void Ex3(int arr[], int n){
 	//Your codes here
-	
-}
-
+	int max = 1, len = 1, maxIndex = 0; 
+	for (int i=1;i<n; i++) {
+if (arr[i] > arr[i-1]) len++; 
+else { if (max < len) {max = len; maxIndex =i-max; } 
+len = 1; } } 
+if (max < len){ max = len; maxIndex = n - max; }
+for (int i=maxIndex; i<max+maxIndex; i++)
+printf("%d",arr[i]); int main();
+int arr[n]; for(int i=0;i<n;i++) 
+Ex3(arr, n); return 0; }
 int main(int argc, char *argv[]) {
 	//testing variable, applying it to your algorithm for auto-evaluating
 	argc--;
